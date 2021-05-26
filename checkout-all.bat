4f0476c228cf4a9f106c5f45ce2c81c94adc5dc2
Tumidanski Justin D <Justin.Tumidanski@dematic.com>
2021-05-26 09:07:57 -0400

    Add a checkout-all script for batch.

---
+++ checkout-all.bat
@@ -0,0 +1,17 @@
+@ECHO OFF
+
+set list="aos" "bns" "cac" "cdc" "cks" "clc" "cos" "csc" "dis" "drg" "eso" "fec" "iis" "marg" "mdc" "mis" "morg" "ncs" "nss" "pos" "sis" "tds" "wcc" "wrg"
+
+cd ..
+(for %%a in (%list%) do (
+   echo atlas-%%a
+   IF EXIST atlas-%%a/docker-build.bat (
+      cd atlas-%%a
+      git pull --no-rebase
+      cd ..
+   ) ELSE (
+      mkdir atlas-%%a
+      git clone https://github.com/jtumidanski/atlas-%%a.git
+   )
+))
+cd atlas
